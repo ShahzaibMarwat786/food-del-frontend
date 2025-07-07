eb900f446b039bde737fab2d1591225fa87cfe06
ShahzaibMarwat786 <muhammadshahzaibhabib4@gmail.com>
2025-07-07 21:50:23 +0500

    ready navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './compomemts/Nav/Navbar'
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
