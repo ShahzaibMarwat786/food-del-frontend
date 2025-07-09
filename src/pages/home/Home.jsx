02df627230e125bc72cf9820c8e033cc6b20280a
ShahzaibMarwat786 <muhammadshahzaibhabib4@gmail.com>
2025-07-09 12:06:34 +0500

    complete header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../compomemts/Header/Header'
+import ExploreMenu from '../../compomemts/ExploreMenu/ExploreMenu'
 
 const Home = () => {
   return (
     <div>
         <Header/>
+        <ExploreMenu/>
     </div>
   )
 }
