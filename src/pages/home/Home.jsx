8ac8755bf4057aae97110c426d3187f29a246fad
ShahzaibMarwat786 <muhammadshahzaibhabib4@gmail.com>
2025-07-09 17:28:50 +0500

    working on footer, create context and itme card

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../compomemts/Header/Header'
 import ExploreMenu from '../../compomemts/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../compomemts/FoodDisplay/FoodDisplay'
 
 const Home = () => {
+
+  const [category, setCategory] = useState("All");
   return (
     <div>
         <Header/>
-        <ExploreMenu/>
+        <ExploreMenu  category={category} setCategory={setCategory}/>
+        <FoodDisplay category={category}/>
     </div>
   )
 }
