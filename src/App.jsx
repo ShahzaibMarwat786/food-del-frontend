c446d9716512d7c9db41a4bb8ebfe69e62b837ac
ShahzaibMarwat786 <muhammadshahzaibhabib4@gmail.com>
2025-07-10 21:07:39 +0500

    complete the home page and make responsive and now work on login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './compomemts/Nav/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Cart from './pages/cart/Cart'
 import PlaceOrder from './pages/place order/PlaceOrder'
 import Footer from './compomemts/Footer/Footer'
+import LoginPopup from './compomemts/LoginPopup/LoginPopup'
 
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false)
   return (
-   <>
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/Cart' element={<Cart/>}/>
-        <Route path='/PlaceOrder' element={<PlaceOrder/>}/>
-      </Routes>
-    </div>
-    <Footer/>
-   </>
+    <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+      <div className='app'>
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Cart' element={<Cart />} />
+          <Route path='/PlaceOrder' element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,15 +3,17 @@ import './Home.css'
 import Header from '../../compomemts/Header/Header'
 import ExploreMenu from '../../compomemts/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../compomemts/FoodDisplay/FoodDisplay'
+import Appdownload from '../../compomemts/AppDownload/Appdownload'
 
 const Home = () => {
 
   const [category, setCategory] = useState("All");
   return (
     <div>
-        <Header/>
-        <ExploreMenu  category={category} setCategory={setCategory}/>
-        <FoodDisplay category={category}/>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
+      <Appdownload />
     </div>
   )
 }
