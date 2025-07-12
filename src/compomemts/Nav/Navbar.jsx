5798645c664f24640af00fee622ad9cc10e2fa85
ShahzaibMarwat786 <muhammadshahzaibhabib4@gmail.com>
2025-07-12 17:30:21 +0500

    working on cart page

--- src/compomemts/Nav/Navbar.jsx
+++ src/compomemts/Nav/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = ({setShowLogin}) => {
 
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt="" className="logo" />
+        <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
         <ul className='navbar-menu'>
             <Link to='/' onClick={()=>setmenu("home")} className={menu=="home"?"active":""}>Home</Link>
             <a href='#explore-menu' onClick={()=>setmenu("menu")} className={menu=="menu"?"active":""}>menu</a>
@@ -19,7 +19,7 @@ const Navbar = ({setShowLogin}) => {
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
+               <Link to='/cart'> <img src={assets.basket_icon} alt="" /></Link>
                 <div className="dot"></div>
             </div>
             <button onClick={()=>setShowLogin(true)}>sing in</button>
