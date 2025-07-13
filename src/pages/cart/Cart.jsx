1c2bbad096fde23a3fd8863de93ee118af83e080
ShahzaibMarwat786 <muhammadshahzaibhabib4@gmail.com>
2025-07-13 14:59:06 +0500

    Complete front-end

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom'
 
 const Cart = () => {
 
-  const {cartItems, food_list, removeFromCart} = useContext(StoreContext)
+  const { cartItems, food_list, removeFromCart, getTotalAmount } = useContext(StoreContext)
+
+  const navigate = useNavigate();
   return (
     <div className='cart'>
       <div className="cart-items">
@@ -15,28 +18,59 @@ const Cart = () => {
           <p>Quantity</p>
           <p>Total</p>
           <p>remove</p>
-           
+
         </div>
         <br />
         <hr />
-        {food_list.map((item,index)=>{
-          if(cartItems[item._id]>0){
-            return(
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
               <div>
                 <div className="cart-items-title cart-items-item">
-                <img src={item.image} alt="" />
-                <p>{item.name}</p>
-                <p>${item.price}</p>
-                <p>{cartItems[item._id]}</p>
-                <p>{item.price*cartItems[item._id]}</p>
-                <p>X</p>
-              </div>
-              <hr />
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p className='cros' onClick={()=>removeFromCart(item._id)}>X</p>
+                </div>
+                <hr />
               </div>
             )
           }
         })
         }
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-totel">
+          <h2>Cart Totels</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>SubTotal</p>
+              <p>${getTotalAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalAmount()===0?0:2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Total</p>
+              <p>${getTotalAmount()===0?0:getTotalAmount()+2}</p>
+            </div>
+          </div>
+          <button onClick={()=>navigate('/PlaceOrder')}>PROCCED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>If you have promo code, Enter it here</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder='Enter promo code' />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
       </div>
 
     </div>
--- src/pages/place order/PlaceOrder.jsx
+++ src/pages/place order/PlaceOrder.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext'
 
 const PlaceOrder = () => {
+
+  const {getTotalAmount} = useContext(StoreContext)
   return (
-    <div>PlaceOrder</div>
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p className='title'>Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='first name'/>
+          <input type="text" placeholder='last name'/>
+        </div>
+        <input type="text" placeholder='Email address' />
+        <input type="text" placeholder='Streets' />
+        <div className="multi-fields">
+          <input type="text" placeholder='City'/>
+          <input type="text" placeholder='State'/>
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder='Zip code'/>
+          <input type="text" placeholder='Country'/>
+        </div>
+        <input type="text" placeholder='Phone' />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-totel">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>SubTotal</p>
+              <p>${getTotalAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalAmount()===0?0:2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Total</p>
+              <p>${getTotalAmount()===0?0:getTotalAmount()+2}</p>
+            </div>
+          </div>
+          <button>PROCCED TO PAYMENT</button>
+        </div>
+      </div>
+    </form>
   )
 }
 
