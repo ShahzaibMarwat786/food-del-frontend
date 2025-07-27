ad1355822decd5f93c93f989118a2f7b8541c9d3
ShahzaibMarwat786 <muhammadshahzaibhabib4@gmail.com>
2025-07-27 14:26:34 +0500

    integrated singup, login and logout functionallity with backend and aslo integrate foodList with backend

--- src/compomemts/LoginPopup/LoginPopup.jsx
+++ src/compomemts/LoginPopup/LoginPopup.jsx
@@ -1,23 +1,60 @@
-import React, { useState } from 'react'
+import React, {  useContext, useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
+import  axios from "axios"
 
 const LoginPopup = ({ setShowLogin }) => {
 
-    const [currentState, setCurrentState] = useState("Sign Up")
+    const {url, setToken} = useContext(StoreContext);
+
+    const [currentState, setCurrentState] = useState("Login")
+    const [data, setData] = useState({
+        name:"",
+        email:"",
+        password:""
+    })
+    const onChangeHandler = (event)=>{
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data=>({...data,[name]:value}))
+    }
+
+    const onLogin = async (event)=>{
+        event.preventDefault();
+        let newUrl = url;
+        if(currentState==="Login"){
+            newUrl += "/api/user/login"
+        }
+        else{
+            newUrl += "/api/user/register"
+        }
+
+        const response = await axios.post(newUrl,data)
+        if(response.data.success){
+            setToken(response.data.token)
+            localStorage.setItem("token",response.data.token)
+            setShowLogin(false)
+        }
+        else{
+            alert(response.data.message)
+        }
+    }
+
+   
     return (
         <div className='login-popup'>
-            <form className="login-popup-container">
+            <form onSubmit={onLogin} className="login-popup-container">
                 <div className="login-popup-titel">
                     <h2>{currentState}</h2>
                     <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
                 </div>
                 <div className="login-popup-inputs">
-                    {currentState === "Login" ? <></> : <input type="text" placeholder='Enter Your name' required />}
-                    <input type="text" placeholder='Enter email' required />
-                    <input type="password" placeholder='Enter Your Password' required />
+                    {currentState === "Login" ? <></> : <input type="text" name='name' onChange={onChangeHandler} value={data.name} placeholder='Enter Your name' required />}
+                    <input type="text" name='email' onChange={onChangeHandler} value={data.email} placeholder='Enter email' required />
+                    <input type="password" name='password' onChange={onChangeHandler} value={data.password} placeholder='Enter Your Password' required />
                 </div>
-                <button>{currentState === "Sign Up" ? "Create account" : "Login"}</button>
+                <button type='submit'>{currentState === "Sign Up" ? "Create account" : "Login"}</button>
                 <div className="login-popup-condition">
                     <input type="checkbox" required />
                     <p>By continuig, I agree to the Terms of Use & Privacy Policy.</p>
